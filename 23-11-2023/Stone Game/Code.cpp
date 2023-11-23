7dd676d17906cbf48fcf4ded9a170e49a440c7bc
devaharish2004 <devaharishm@gmail.com>
2023-11-23 11:09:41 +0530

    Stone game

---
+++ 23-11-2023/Stone Game/Code.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        if(n%4 == 1)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+}
